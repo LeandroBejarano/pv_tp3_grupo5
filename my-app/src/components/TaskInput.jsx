46c48581f6b961a4594fc5b76698d42798f8bea4
Leandro794 <44125429@fi.unju.edu.ar>
2025-04-30 19:57:05 -0300

    fix: Corrección de funciones y variables recibidas [Leandro Bejarano]

--- my-app/src/components/TaskInput.jsx
+++ my-app/src/components/TaskInput.jsx
@@ -1,9 +1,21 @@
 function TaskInput(props){
     const [tarea, setTarea] = props.elemento;
+    const [tareas, setTareas] = props.tareas;
+
+
+    const handleAdd = () =>{
+        if (tarea && !tareas.includes(tarea)){
+            setTareas([...tareas, tarea]);
+            setTarea("");
+        }
+        else
+            alert("Error, tarea ya registrada");
+    };
+
     return (
         <div>
             <input style={{height:30, textAlign:"center"}} type="text" value={tarea} onChange={e => setTarea(e.target.value)} placeholder="Ingresar tarea" />
-            <button style={{padding:5, height:35, marginLeft:15}} >Guardar tarea</button>
+            <button style={{padding:5, height:35, marginLeft:15}} onClick={handleAdd}  >Guardar tarea</button>
         </div>
     );
 }
