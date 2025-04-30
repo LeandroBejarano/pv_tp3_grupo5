3b381df414b9accc77a7551395a123eef643ef16
Leandro794 <44125429@fi.unju.edu.ar>
2025-04-30 19:55:33 -0300

    feat: Agregado de TaskList al script [Leandro Bejarano] fix: Corrección de variables enviadas a TaskInput [Leandro Bejarano]

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 import TaskInput from "./components/TaskInput"
+import TaskList from "./components/TaskList"
 
 function App() {
+  const [tareas, setTareas] = useState([]);
   const [tarea, setTarea] = useState('');
   return (
     <>
       <div>
-      <TaskInput elemento={[tarea, setTarea]}></TaskInput>
+        <TaskInput elemento={[tarea, setTarea]} tareas={[tareas, setTareas]}></TaskInput>
+        <TaskList tareas={tareas}></TaskList>
       </div>
     </>
   )
