7448a8276c6c46f6d34a0e2edbeb73ef7d63e48b
Leandro794 <44125429@fi.unju.edu.ar>
2025-04-30 20:40:07 -0300

    fix: Corrección de datos enviados a TaskList [Leandro Bejarano]

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div>
         <TaskInput elemento={[tarea, setTarea]} tareas={[tareas, setTareas]}></TaskInput>
-        <TaskList tareas={tareas}></TaskList>
+        <TaskList tareas={tareas} setTareas={setTareas}></TaskList>
       </div>
     </>
   )
