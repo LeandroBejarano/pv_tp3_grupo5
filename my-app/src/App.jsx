77c087915c961227250e2f280a4b9a04e54f568b
TorresNahuelTobias <tobiastorres123456@gmail.com>
2025-05-01 19:02:23 -0300

    fix: incorporación del componete Taskltem

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import TaskInput from "./components/TaskInput"
 import TaskList from "./components/TaskList"
+import TaskItem from "./components/TaskItem";
 
 function App() {
   const [tareas, setTareas] = useState([]);
@@ -10,6 +11,11 @@ function App() {
       <div>
         <TaskInput elemento={[tarea, setTarea]} tareas={[tareas, setTareas]}></TaskInput>
         <TaskList tareas={tareas} setTareas={setTareas}></TaskList>
+        <TaskItem
+          key={task.id}
+          task={task}
+          onToggleComplete={() => {}}
+          onDelete={() => {}}></TaskItem>
       </div>
     </>
   )
