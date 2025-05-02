143a0b012f1b6d1eb43dede8621ccfb286d549ea
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-01 21:00:52 -0300

    fix: Corrección, una función no se usaba [Leandro Bejarano]

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react"
-import TaskInput from "./components/TaskInput"
-import TaskList from "./components/TaskList"
+import { useState } from "react";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
 import TaskItem from "./components/TaskItem";
 
 function App() {
   const [tareas, setTareas] = useState([]);
   const [tarea, setTarea] = useState('');
   const [busqueda, setBusqueda] = useState('');
+
   return (
     <>
       <div>
         <TaskInput elemento={[tarea, setTarea]} tareas={[tareas, setTareas]}></TaskInput>
-        <TaskList tareas={tareas} setTareas={setTareas}></TaskList>
-        <TaskItem onSearch={handleSearch}></TaskItem> 
+        <TaskItem setBusqueda={setBusqueda} />
+        <TaskList tareas={tareas} setTareas={setTareas} busqueda={busqueda} />
       </div>
     </>
   )
