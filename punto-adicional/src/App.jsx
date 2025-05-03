fbfb823b0a706c59c2509c6ec80688f48d087ffa
Leandro794 <44125429@fi.unju.edu.ar>
2025-05-02 21:03:51 -0300

    feat: Declaración del array de objetos

--- punto-adicional/src/App.jsx
+++ punto-adicional/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 
 function App() {
+  const [productos, setProductos] = useState([
+    { descripcion: "Auriculares Inalámbricos", precio: 49000.50 },
+    { descripcion: "Mouse Óptico", precio: 15000 },
+    { descripcion: "Monitor 24 Pulgadas", precio: 120000 },
+    { descripcion: "Webcam HD", precio: 35000.75 },
+    { descripcion: "Parlante Estéreo", precio: 28000 },
+]);
   return (
     <>
 
